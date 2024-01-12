39d91b4408a33b87813af27c6dd341a72eb64936
AgneDJ <fizikee@gmail.com>
2024-01-11 23:46:05 -0800

    Login_signup forms handling

--- server.py
+++ server.py
@@ -4,53 +4,77 @@ from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
 import crud
 
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
 def homepage():
     """View homepage."""
-    name = request.args.get('user_name')
+    # name = request.args.get('user_name')
     return render_template("homepage.html")
 
 
 @app.route('/profile')
 def rendering_profile():
-    """Greet user."""
-    name = request.args.get('user_name')
-    if not name:
-        return redirect("/signin")
+    """User profile."""
+    if "user_email" not in session:
+        return redirect("/signup")
+    email = session["user_email"]
+    user = crud.get_user_by_email(email)
+# tghjk
+    return render_template("profile.html", name=user.user_name)
 
-    return render_template("profile.html", name=name)
 
-
-@app.route("/signin")
-def signing_up():
-    """View sign up form."""
-
+@app.route("/signup")
+def show_register_user():
     return render_template("signin.html")
 
 
-@app.route("/signin", methods=["POST"])
+@app.route("/signup", methods=["POST"])
 def register_user():
-    """Create a new user."""
+    """Register user."""
 
     name = request.form.get("name")
     email = request.form.get("email")
     password = request.form.get("password")
 
-    user = crud.get_user_by_email(name)
+    user = crud.get_user_by_email(email)
 
     if user:
-        flash("The user with this name already exists. Try again.")
+        flash("Cannot create an account with that email. Try again.")
+        return redirect("/signup")
     else:
         user = crud.create_user(name, email, password)
         db.session.add(user)
         db.session.commit()
-        flash("Great! Your account has been created! Please log in.")
+        flash("Account created! Please log in.")
 
     return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def logging_in():
+    """Logging user in."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print(email, password)
+
+    user = crud.get_user_by_email(email)
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    print(user)
+# sup
+    if not user or user.user_password != password:
+        flash("Email or password are incorrect, my friend.")
+        return redirect("/")
+    else:
+        session["user_email"] = user.user_email
+        flash(f"Hey there, {user.user_email}, what's up?")
+    return redirect("/profile")
 
 
 if __name__ == "__main__":
